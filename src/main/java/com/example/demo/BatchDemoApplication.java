04c436ed771f94f535400547f268a62e3e738821
vivek-sundli <viveksundli@gmail.com>
2024-01-23 21:03:26 +0530

    creating batch project

---
+++ src/main/java/com/example/demo/BatchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchDemoApplication.class, args);
+		System.out.println("Hello!");
+	}
+}
